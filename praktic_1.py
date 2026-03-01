db3cc054be2106573fb048a04dc5194cf47e216d
Derek <ushakov_gd@mail.ru>
2026-03-01 23:44:06 +0300

    Изменил не спец. метод (изменения скорости поездки на данный момент). Буду добавлять 2 новых (читай далее)

--- praktic_1.py
+++ praktic_1.py
@@ -8,6 +8,7 @@
 # скорости. Операция вывода на экран ( ) должна аккумулировать состояние полей объекта/
 
 class AutoModel:
+    MAX_SPEED = 200
 
     brand: str
     model: str
@@ -33,6 +34,12 @@ class AutoModel:
             self.state = "заглушен"
             self.speed = 0
 
+        if not 0 < self.speed or self.speed < self.MAX_SPEED:
+            print (f"Скорость не может быть меньше 0 или больше {self.MAX_SPEED} км/ч")
+
+        else:
+            print(f"Скорость {self.speed} введена верно")
+
     def __str__(self):
         return f"""Автомобиль марки: {self.brand} {self.model}. Год выпуска {self.year}, цвет {self.color}.
 Пробег {self.car_mileage_km} км, состояние {self.state}. Скорость {self.speed} км/ч."""
@@ -45,7 +52,7 @@ class AutoModel:
 
         while Flag:
 
-            start = input("Нажмите s >")
+            start = input("Нажмите s > ")
 
             if start == "s":
                 self.state = True
@@ -53,9 +60,49 @@ class AutoModel:
                 return "Двигатель запущен"
 
             else:
-                print("Неудачный запуск двигателя (жми нужную кнопку!)"
+                print("Неудачный запуск двигателя (жми нужную кнопку!)")
+
+    def change_speed(self, current_speed: int): #метод изменения скорости поездки на данный момент
+
+        if not current_speed <= self.MAX_SPEED or not current_speed > 0:
+            return f"Скорость не может быть отрицательной или превышать допустимый порог в {self.MAX_SPEED} км/ч"
+
+        else:
+            self.speed = current_speed
+            print(f"Поездка происходит на скорости {self.speed} км/ч")
+
+        is_speed_info = input("""Что хотим увеличить или сбросить скорость?
+Жми на селекторе "+" для увеличения. Либо "-" для сброса скорости > """)
+
+        info = print(f"Движемся на скорости {self.speed} км/ч")
+
+        Flag = True
+        while Flag:
+
+            if is_speed_info == "+":
+                speed_plus = int(input("На сколько увеличиваем > "))
+
+                if (self.speed + speed_plus) < 201:
+                    self.speed = (speed_plus + self.speed)
+                    Flag = False
+                    print(f"Движемся на скорости {self.speed} км/ч")
+
+                else:
+                    print("Скорость не может превышать 200 км/ч")
+
+            elif is_speed_info == "-":
+                speed_minus = int(input("На сколько снижаем > "))
+
+                if (self.speed - speed_minus) > 0:
+                    self.speed = (self.speed - speed_minus)
+                    Flag = False
+                    print(info)
+
+        return f"Опосля изменения, движемся на скорости {self.speed} км/ч"
 
     def stop_engine(self): #метод глушения двигателя
+
+        info = "Двигатель заглушен"
 
         print("Перед выходом из автомобиля, глушим двигатель")
 
@@ -69,18 +116,20 @@ class AutoModel:
                 if stop == "s":
                     self.state = False
                     Flag = False
-                    return "Двигатель заглушен"
+                    return info
 
                 else:
                     print("Опять ищи нужную кнопку")
 
             else:
-                print("Камикадзе, сначала затормози!")
+                self.speed = 0
+                self.state = False
+                return info
 
-    def change_speed(self):
-        pass
 
-    def change_color(self): #метод изменения скорости
+
+
+    def change_color(self): #метод изменения цвета
         pass
 
     def is_running_engine(self): #метод запроса -запущен ли двигатель?
@@ -94,5 +143,7 @@ a = AutoModel("BMW", "X600", 2020, "Белый", 107000, False, 0)
 print(a)
 start = a.starting_engine()
 print(start)
+ch = a.change_speed(120)
+print(ch)
 stop = a.stop_engine()
 print(stop)
